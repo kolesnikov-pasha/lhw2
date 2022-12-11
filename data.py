f6661d1b032c23a9da83b103d54abe7e79219414
Kolesnikov Pavel <olpyhn@gmail.com>
2022-12-11 01:44:27 +0100

    first commit

---
+++ data.py
@@ -0,0 +1,149 @@
+from enum import Enum
+from pathlib import Path
+import bs4 as bs
+
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+
+def process_training_file(input_path: Path, output_path: Path):
+    result = {}
+    with open(input_path, "r") as source:
+        html = bs.BeautifulSoup(source)
+        for doc in html.find_all("doc"):
+            try:
+                text = list(filter(lambda s: s != None and s != '\n' and not str(s).startswith("<"), doc.contents))[0]
+            except:
+                text = ""
+            result[int(doc.talkid.string)] = text
+    max_index = max(list(result.keys()))
+    with open(output_path, "w") as output:
+        for i in range(max_index):
+            texts = result.get(i + 1, "").split("\n")
+            for text in texts:
+                output.write(f"{text.strip()}\n")
+
+def process_evaluation_file(input_path: Path, output_path: Path):
+    result = []
+    with open(input_path, "r") as source:
+        html = bs.BeautifulSoup(source)
+        for doc in html.find_all("doc"):
+            for seg in doc.find_all("seg"):
+                result.append(seg.string + "\n")
+    with open(output_path, "w") as output:
+        output.writelines(result)
+
+
+def convert_files(base_path: Path, output_path: Path):
+    """
+    Given a directory containing all the dataset files, convert each one into the "one example per line" format.
+    :param base_path: Path containing files with original data
+    :param output_path: Path containing files with processed data
+    """
+
+    for language in "de", "en":
+        process_training_file(
+            base_path / f"train.tags.de-en.{language}",
+            output_path / f"train.{language}.txt",
+        )
+        process_evaluation_file(
+            base_path / f"IWSLT17.TED.dev2010.de-en.{language}.xml",
+            output_path / f"val.{language}.txt",
+        )
+        process_evaluation_file(
+            base_path / f"IWSLT17.TED.tst2010.de-en.{language}.xml",
+            output_path / f"test.{language}.txt",
+        )
+
+
+class SpecialTokens(Enum):
+    UNKNOWN = "[UNK]"
+    PADDING = "[PAD]"
+    BEGINNING = "[BOS]"
+    END = "[EOS]"
+
+
+class TranslationDataset(Dataset):
+    def __init__(
+        self,
+        src_file_path,
+        tgt_file_path,
+        src_tokenizer: Tokenizer,
+        tgt_tokenizer: Tokenizer,
+        max_len=32,
+    ):
+        """
+        Loads the training dataset and parses it into separate tokenized training examples.
+        No padding should be applied at this stage
+        :param src_file_path: Path to the source language training data
+        :param tgt_file_path: Path to the target language training data
+        :param src_tokenizer: Trained tokenizer for the source language
+        :param tgt_tokenizer: Trained tokenizer for the target language
+        :param max_len: Maximum length of source and target sentences for each example:
+        if either of the parts contains more tokens, it needs to be filtered.
+        """
+        with open(src_file_path, "r") as src:
+            src_lines = src.readlines()
+        with open(tgt_file_path, "r") as tgt:
+            tgt_lines = tgt.readlines()
+        self.examples = []
+        for i in tqdm(range(min(len(src_lines), len(tgt_lines)))):
+            src = src_lines[i]
+            tgt = tgt_lines[i]
+            if len(src) == 0 or len(tgt) == 0:
+                continue
+            src_tokens = src_tokenizer.encode(src).ids
+            tgt_tokens = tgt_tokenizer.encode(tgt).ids
+            if len(src_tokens) > max_len or len(tgt_tokens) > max_len:
+                continue
+            self.examples.append((src_tokens, tgt_tokens))
+
+        self.end_src = src_tokenizer.encode(SpecialTokens.END.value).ids[0]
+        self.pad_src = src_tokenizer.encode(SpecialTokens.PADDING.value).ids[0]
+        self.end_tgt = tgt_tokenizer.encode(SpecialTokens.END.value).ids[0]
+        self.pad_tgt = tgt_tokenizer.encode(SpecialTokens.PADDING.value).ids[0]
+        self.max_len = max_len
+
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i):
+        return self.examples[i]
+
+    def collate_translation_data(self, batch):
+        """
+        Given a batch of examples with varying length, collate it into `source` and `target` tensors for the model.
+        This method is meant to be used when instantiating the DataLoader class for training and validation datasets in your pipeline.
+        """
+        source = []
+        target = []
+        for example in batch:
+            src = example[0] + (self.max_len - len(example[0])) * [self.pad_src]
+            tgt = example[1] + (self.max_len - len(example[1])) * [self.pad_tgt]
+            source.append(src)
+            target.append(tgt)
+        return source, target
+
+
+def train_tokenizer(train_file: Path, validation_file: Path, save_dir):
+    tokenizer = Tokenizer(BPE())
+    tokenizer.pre_tokenizer = Whitespace()
+    trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"], show_progress=True, vocab_size=30000)
+    tokenizer.train(files=[str(train_file.absolute()), str(validation_file.absolute())], trainer=trainer)
+    tokenizer.save(str(save_dir))
+
+
+def train_tokenizers(base_dir: Path, save_dir: Path):
+    """
+    Trains tokenizers for source and target languages and saves them to `save_dir`.
+    :param base_dir: Directory containing processed training and validation data (.txt files from `convert_files`)
+    :param save_dir: Directory for storing trained tokenizer data (two files: `tokenizer_de.json` and `tokenizer_en.json`)
+    """
+    train_tokenizer(base_dir / "train.de.txt", base_dir / "val.de.txt", save_dir / "tokenizer_de.json")
+    train_tokenizer(base_dir / "train.en.txt", base_dir / "val.en.txt", save_dir / "tokenizer_en.json")
---
+++ model.py
@@ -0,0 +1,133 @@
+
+import math
+from typing import Tuple
+
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim_model, dropout_p, max_len):
+        super().__init__()
+        # Modified version from: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+        # max_len determines how far the position can have an effect on a token (window)
+        
+        # Info
+        self.dropout = nn.Dropout(dropout_p)
+        
+        # Encoding - From formula
+        pos_encoding = torch.zeros(max_len, dim_model)
+        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4, 5
+        division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model) # 1000^(2i/dim_model)
+        
+        # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
+        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
+        
+        # PE(pos, 2i + 1) = cos(pos/1000^(2i/dim_model))
+        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
+        
+        # Saving buffer (same as parameter without gradients needed)
+        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pos_encoding",pos_encoding)
+        
+    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
+        # Residual connection + pos encoding
+        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
+
+
+class TranslationModel(nn.Module):
+    def __init__(
+        self,
+        num_encoder_layers: int = 4,
+        num_decoder_layers: int = 4,
+        dim_model: int = 512,
+        dim_feedforward: int = 2048,
+        n_head: int = 4,
+        src_vocab_size: int = 30000,
+        tgt_vocab_size: int = 30000,
+        dropout_prob: float = 0.1,
+    ):
+        """
+        Creates a standard Transformer encoder-decoder model.
+        :param num_encoder_layers: Number of encoder layers
+        :param num_decoder_layers: Number of decoder layers
+        :param emb_size: Size of intermediate vector representations for each token
+        :param dim_feedforward: Size of intermediate representations in FFN layers
+        :param n_head: Number of attention heads for each layer
+        :param src_vocab_size: Number of tokens in the source language vocabulary
+        :param tgt_vocab_size: Number of tokens in the target language vocabulary
+        :param dropout_prob: Dropout probability throughout the model
+        """
+        super().__init__()
+        self.model_type = "Transformer"
+        self.dim_model = dim_model
+        self.dropout = dropout_prob
+
+        # LAYERS
+        self.positional_encoder = PositionalEncoding(
+            dim_model=self.dim_model, dropout_p=self.dropout, max_len=50
+        )
+        self.embedding_src = nn.Embedding(src_vocab_size, self.dim_model)
+        self.embedding_tgt = nn.Embedding(tgt_vocab_size, self.dim_model)
+        self.transformer = nn.Transformer(
+            d_model=self.dim_model,
+            nhead=n_head,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dim_feedforward=dim_feedforward,
+            dropout=self.dropout,
+        )
+        self.out = nn.Linear(dim_model, tgt_vocab_size)
+
+
+    def forward(
+        self,
+        src_tokens: Tensor,
+        tgt_tokens: Tensor,
+        tgt_mask: Tensor,
+        src_padding_mask: Tensor,
+        tgt_padding_mask: Tensor,
+    ):
+        """
+        Given tokens from a batch of source and target sentences, predict logits for next tokens in target sentences.
+        """
+        src = self.embedding_src(src_tokens) * math.sqrt(self.dim_model)
+        tgt = self.embedding_tgt(tgt_tokens) * math.sqrt(self.dim_model)
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+        
+        # We could use the parameter batch_first=True, but our KDL version doesn't support it yet, so we permute
+        # to obtain size (sequence length, batch_size, dim_model),
+        src = src.permute(1,0,2)
+        tgt = tgt.permute(1,0,2)
+
+        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_padding_mask, tgt_key_padding_mask=tgt_padding_mask)
+        out = self.out(transformer_out)
+        
+        return out
+
+
+    def get_tgt_mask(self, size) -> torch.tensor:
+        # Generates a squeare matrix where the each row allows one word more to be seen
+        mask = torch.tril(torch.ones(size, size) == 1) # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf')) # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0)) # Convert ones to 0
+        
+        # EX for size=5:
+        # [[0., -inf, -inf, -inf, -inf],
+        #  [0.,   0., -inf, -inf, -inf],
+        #  [0.,   0.,   0., -inf, -inf],
+        #  [0.,   0.,   0.,   0., -inf],
+        #  [0.,   0.,   0.,   0.,   0.]]
+        
+        return mask
+    
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        # If matrix = [1,2,3,0,0,0] where pad_token=0, the result mask is
+        # [False, False, False, True, True, True]
+        return (matrix == pad_token)
